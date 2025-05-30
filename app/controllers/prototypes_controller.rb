d585c0504bc055bc4446398da54fbebd7e51561d
Kengineer <61613215+kengineer923@users.noreply.github.com>
2025-05-30 16:48:34 +0900

    名前から詳細ページへ遷移と編集、削除ボタンの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -53,9 +53,10 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
+  def ensure_correct_user
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path, alert: "権限がありません"
     end
   end
 end
